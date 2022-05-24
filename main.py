8d566454973d162b9bbc4472ed50516dd2eebd35
megan <megan.pettipher@hotmail.com>
2022-05-24 19:38:29 +0100

    "fizzbuzz while loop/class...IT WORKS YAYYY"

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 
 print("Welcome to Fizzbuzz!")
-guess = int(input("please choose a number to start with\n"))
 
-max_number = int(input("choose a maximum number\n"))
-game_number = True
-while game_number:
-    for i in range(1, max_number):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+
+def fizzbuzz():
+    max_number = int(input("choose a maximum number\n"))
+    game_number = True
+    while game_number:
+        num = int(input("choose a number to start with\n"))
+        for i in range(num, max_number):
+            if i % 3 == 0 and i % 5 == 0:
+             print("FizzBuzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+    return fizzbuzz()
+
+
+print(fizzbuzz())
 print("thank you for playing fizzbuzz!")
